91ecea5816b25c45b73febb662a632cf009b2c01
teleskafranco <165854240+teleskafranco@users.noreply.github.com>
2025-04-15 22:07:26 +0000

    Criei as variaveis da carta 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,9 +7,15 @@
 //Teste larissa
 
 int main() {
+    // Variáveis para a Carta 1
     char Estado1[50], Codigo1[50], cidade1[50];
     int Populacao1;
     float Area1, PIB1, Pontos_Turisticos1;
+
+    // Variáveis para a Carta 2
+    char Estado2[50], Codigo2[50], cidade2[50];
+    int Populacao2;
+    float Area2, PIB2, Pontos_Turisticos2;
 
     return 0;
 }
