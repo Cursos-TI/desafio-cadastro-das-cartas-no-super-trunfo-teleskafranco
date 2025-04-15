d4ad287005624272ce536ba5b020548ac12e267d
teleskafranco <165854240+teleskafranco@users.noreply.github.com>
2025-04-15 22:06:07 +0000

    Criei as variaveis da carta 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,16 +7,9 @@
 //Teste larissa
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    char Estado1[50], Codigo1[50], cidade1[50];
+    int Populacao1;
+    float Area1, PIB1, Pontos_Turisticos1;
 
     return 0;
 }
